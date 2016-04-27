b90ad04c19ff4b0a1ce778e5dc48492fb3872eae
Timothy Barrett <liltimtim@gmail.com>
2016-04-26 22:08:01 -0400

    commented out test code to save an object to parse

--- OUAS/OUAS/ViewController.swift
+++ OUAS/OUAS/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let object = PFObject(className: "Test", dictionary: ["test":"object"])
-        object.saveInBackgroundWithBlock { (success, error) in
-            print(error)
-            print("success \(success)")
-        }
+//        let object = PFObject(className: "Test", dictionary: ["test":"object"])
+//        object.saveInBackgroundWithBlock { (success, error) in
+//            print(error)
+//            print("success \(success)")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
