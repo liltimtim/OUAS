1734de1ceb80f28395052f9681833e4d4346f0ca
Timothy Barrett <liltimtim@gmail.com>
2016-05-24 20:56:02 -0400

    removed the old models

--- OUAS/OUAS/GameStore.swift
+++ OUAS/OUAS/GameStore.swift
@@ -2,103 +2,44 @@
 //  GameStore.swift
 //  OUAS
 //
-//  Created by Timothy Barrett on 5/23/16.
+//  Created by Timothy Barrett on 5/24/16.
 //  Copyright © 2016 Timothy Barrett. All rights reserved.
 //
 
 import Foundation
 import Parse
 class GameStore: NSObject {
-    static let shared:GameStore = GameStore()
-    private(set) var currentUser:PFUser?
-    private override init () {
-        
-    }
+    static let shared : GameStore = GameStore()
+    private(set) var currentUser:PFUser!
     
-    // MARK: User Functions
+    private override init () { super.init() }
     
-    func authenticate(withUsername username:String, withPassword password:String, completion:(user:PFUser?, error:NSError?)->Void) {
+    func authenticate(username:String, password:String, completion:(currentUser:PFUser?, error:NSError?)->Void) {
         PFUser.logInWithUsernameInBackground(username, password: password) { (user, error) in
-            self.currentUser = user
-            
-            if user?.objectId != nil {
-                let tokenObject = PFObject(className: "DeviceTokens")
-                tokenObject["user_id"] = user!.objectId!
-                tokenObject["installation"] = PFInstallation.currentInstallation()
-                tokenObject.saveInBackground()
+            if error == nil {
+                if user != nil {
+                    self.currentUser = user!
+                    completion(currentUser: user, error: nil)
+                } else {
+                    completion(currentUser: nil, error: NSError(domain: "GameStore", code: 0, userInfo: ["error":"Could not authenticate user"]))
+                }
+            } else {
+                completion(currentUser: nil, error: error)
             }
-            completion(user: user, error: error)
         }
     }
     
-    func signUp(withUsername username:String, withEmail email:String, withPassword password:String, completion:(user:PFUser?, error:NSError?)->Void) {
+    func signUp(email:String, username:String, password:String, completion:(currentUser:PFUser?, error:NSError?)->Void) {
         let user = PFUser()
         user.username = username
         user.password = password
         user.email = email
         user.signUpInBackgroundWithBlock { (success, error) in
-            self.currentUser = PFUser.currentUser()
-            completion(user: PFUser.currentUser(), error: error)
-        }
-    }
-    
-    /*
-    func addFriend(withPlayerObject player:Player, completion:(success:Bool, error:NSError?)->Void) {
-        
-    }
-    
-    func removeFriend(withPlayerObject player:Player, completion:(success:Bool, error:NSError?)->Void) {
-        
-    }
-    */
-    
-    func createGame(withOwner owner:Player, completion:(gameObject:GameRoom?, error:NSError?)->Void) {
-        let newRoom = GameRoom(withOwner: owner)
-        let object = newRoom.toPFObject()
-        print(object)
-        object.saveInBackgroundWithBlock { (result, error) in
-            if result {
-                completion(gameObject: newRoom, error: nil)
+            if success {
+                completion(currentUser: user, error: nil)
             } else {
-                completion(gameObject: nil, error: error)
+                completion(currentUser: nil, error: error)
             }
         }
     }
-    
-    func findPlayers(completion:(players:[Player]?, error:NSError?)->Void) {
-        let query = PFUser.query()
-        query?.findObjectsInBackgroundWithBlock({ (objects, error) in
-            if error == nil {
-                if objects != nil {
-                    var players = [Player]()
-                    for player in objects! {
-                        if let aPlayer = Player.fromPFObject(player) {
-                            players.append(aPlayer)
-                        }
-                    }
-                    completion(players: players, error: nil)
-                } else {
-                    completion(players: nil, error: nil)
-                }
-            } else {
-                completion(players: nil, error: error)
-            }
-        })
-    }
-    
-    func invitePlayer(withUsername username:String, completion:(player:Player?, error:NSError?)->Void) {
-        if let query = PFUser.query() {
-            query.whereKey("username", equalTo: username)
-            query.getFirstObjectInBackgroundWithBlock({ (object, error) in
-                if object != nil && error == nil {
-                    completion(player: Player.fromPFObject(object!), error: error)
-                } else {
-                    completion(player: nil, error: error)
-                }
-            })
-        } else {
-            completion(player: nil, error: NSError(domain: "GameStore", code: 0, userInfo: ["error":"Could not setup query"]))
-        }
-    }
-    
 }
--- OUAS/OUASTests/OUASTests.swift
+++ OUAS/OUASTests/OUASTests.swift
@@ -10,140 +10,20 @@ import XCTest
 import Parse
 @testable import OUAS
 
-
-class GameRoomTests : XCTestCase {
-    func testCreateGame() {
-        let exp = expectationWithDescription("Create a game")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            XCTAssertNotNil(user)
-            XCTAssertNil(error)
-            print(user)
-            print(error)
-            let player = Player.fromPFObject(user!)!
-            GameStore.shared.createGame(withOwner: player, completion: { (gameObject, error) in
-                XCTAssertNotNil(gameObject)
-                XCTAssertNil(error)
-                exp.fulfill()
-            })
-        }
-        waitForExpectationsWithTimeout(100, handler: nil)
+class UserTests : XCTestCase {
+    func testAuthenticateUser() {
+        
     }
     
-    func testCreateGameAddContent() {
-        let exp = expectationWithDescription("Create a game and add content")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            XCTAssertNotNil(user)
-            XCTAssertNil(error)
-            print(error)
-            let player = Player.fromPFObject(user!)!
-            GameStore.shared.createGame(withOwner: player, completion: { (gameObject, error) in
-                XCTAssertNotNil(gameObject)
-                XCTAssertNil(error)
-                print(gameObject)
-                gameObject!.postNewContent(withContent: "Hello world content", withContentOwner: user!, completion: { (success, error) in
-                    XCTAssertTrue(success)
-                    XCTAssertNil(error)
-                    exp.fulfill()
-                })
-            })
-        }
-        waitForExpectationsWithTimeout(40, handler: nil)
+    func testSignUpUser() {
+        
     }
     
-    func testCreateGameRoomAddContentVerifyContentAdded() {
-        let exp = expectationWithDescription("Create a game and add content")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            XCTAssertNotNil(user)
-            XCTAssertNil(error)
-            print(error)
-            let player = Player.fromPFObject(user!)!
-            GameStore.shared.createGame(withOwner: player, completion: { (gameObject, error) in
-                XCTAssertNotNil(gameObject)
-                XCTAssertNil(error)
-                print(gameObject)
-                gameObject!.postNewContent(withContent: "Hello world content", withContentOwner: user!, completion: { (success, error) in
-                    XCTAssertTrue(success)
-                    XCTAssertNil(error)
-                    gameObject?.getGameContent({ (content, error) in
-                        XCTAssertNotNil(content)
-                        XCTAssertGreaterThan(content!.count, 0)
-                        exp.fulfill()
-                    })
-                })
-            })
-        }
-        waitForExpectationsWithTimeout(40, handler: nil)
+    func testGetPreviousGames() {
+        
     }
     
-    func testCreateGameAddOpponent() {
-        let exp = expectationWithDescription("Create a game and add opponent")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            let player = Player.fromPFObject(user!)!
-            GameStore.shared.createGame(withOwner: player, completion: { (gameObject, error) in
-                gameObject?.addOpponent(withPlayerObject: player, completion: { (success, error) in
-                    XCTAssertTrue(success)
-                    XCTAssertNil(error)
-                    exp.fulfill()
-                })
-            })
-        }
-        waitForExpectationsWithTimeout(400, handler: nil)
-    }
-    
-    func testStartGameAndEndGame() {
-        let exp = expectationWithDescription("Start and End a game")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            let player = Player.fromPFObject(user!)!
-            GameStore.shared.createGame(withOwner: player, completion: { (gameObject, error) in
-                XCTAssertNotNil(gameObject)
-                gameObject?.endGame({ (success, error) in
-                    XCTAssertTrue(success)
-                    XCTAssertNil(error)
-                    exp.fulfill()
-                })
-            })
-        }
-        waitForExpectationsWithTimeout(40, handler: nil)
-    }
-    
-    func testInvitePlayer() {
-        let exp = expectationWithDescription("Invite player to game")
-        GameStore.shared.invitePlayer(withUsername: "liltimtim") { (player, error) in
-            XCTAssertNotNil(player)
-            XCTAssertNil(error)
-            exp.fulfill()
-        }
-        waitForExpectationsWithTimeout(30, handler: nil)
-    }
-    
-    func testAuthenticate() {
-        let exp = expectationWithDescription("Test Authenticate User")
-        GameStore.shared.authenticate(withUsername: "liltimtim", withPassword: "password1") { (user, error) in
-            XCTAssertNil(error)
-            XCTAssertNotNil(user)
-            exp.fulfill()
-        }
-        waitForExpectationsWithTimeout(10, handler: nil)
-    }
-    
-    func testCreateUser() {
-        let exp = expectationWithDescription("test authentication")
-        GameStore.shared.signUp(withUsername: "liltimtim", withEmail: "liltimtim@gmail.com", withPassword: "password1") { (user, error) in
-            XCTAssertNotNil(user)
-            XCTAssertNil(error)
-            exp.fulfill()
-        }
-        waitForExpectationsWithTimeout(10, handler: nil)
-    }
-    
-    func testFindAvailableUsers() {
-        let exp = expectationWithDescription("Get available Users")
-        GameStore.shared.findPlayers { (players, error) in
-            XCTAssertNotNil(players)
-            XCTAssertNil(error)
-            XCTAssertGreaterThan(players!.count, 0)
-            exp.fulfill()
-        }
-        waitForExpectationsWithTimeout(10, handler: nil)
+    func testGetChallenges() {
+        
     }
 }
